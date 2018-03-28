a773d6f9d475c997c5e77e5358049fdcbe09eb87
JunYu Lai <chaojisuxiaowoniu@163.com>
2018-03-28 11:40:54 +0800

    2nd

--- app/src/main/java/com/example/arthurlai/ever/FindWordActivity.java
+++ app/src/main/java/com/example/arthurlai/ever/FindWordActivity.java
@@ -2,6 +2,8 @@ package com.example.arthurlai.ever;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class FindWordActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class FindWordActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_word);
+    }
+
+    public void deleteWord (View view) {
+        EditText editText = (EditText) findViewById(R.id.edit_text);
+        editText.setText("");
     }
 }
--- app/src/main/java/com/example/arthurlai/ever/MainActivity.java
+++ app/src/main/java/com/example/arthurlai/ever/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.arthurlai.ever;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // 这个方法打开FindWordActivity
+    public void findWord(View view) {
+        Intent intent = new Intent(this, FindWordActivity.class);
+        startActivity(intent);
     }
 }
